5f3d8dd20dab304c8cb39c5616be3f33d4cc4e12
pndgoria <kartashova.na09@gmail.com>
2022-01-17 01:53:36 +0300

    minor fixes

--- mini_tester/generator.py
+++ mini_tester/generator.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import random
+import sys
 
 def main():
-    numbers = random.sample(range(-100, 900), 100)
+    number = sys.argv[1]
+    numbers = random.sample(range(-100, 900), int(number))
     random.shuffle(numbers)
     for j in numbers:
         print(j, end=' ')
--- mini_tester/tester.sh
+++ mini_tester/tester.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+num=100
+echo "20 тестов для $num случайных чисел"
 cd ../
 make &> /dev/null
 i=0
 echo "___________________________________________"
 while [ $i -le 20 ] ;
-do ./push_swap $(mini_tester/generator.py) | wc -l
+do ./push_swap $(mini_tester/generator.py $num) | wc -l
 i=$(($i + 1)) ;
 done
 echo "____________________________________________"
